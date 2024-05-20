80711ce4f91cf8667e408652cfb86c6d01ac1a82
Hailey Kim <120682066+hailey-kim-26@users.noreply.github.com>
2024-05-20 03:16:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+    Targeting menu and icon elements to open the menu bar when clicked
+*/
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".mobile-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+/*
+    Typewriter effect and blinking cursor in Text line
+*/
+const firstText = "cat README";
+const secondText = [
+    "Name: Hailey Kim",
+    "Major: B.S. Computer Science",
+    "Minor: Mathematics",
+    "School: University of Nevada-Las Vegas",
+    "hailey@desktop:~$ "
+];
+
+let index = 0; // track current position in the firstText
+const speed = 100; // speed in milliseconds for typing effect
+
+function typeWriter() {
+    if (index < firstText.length) {
+        document.getElementById("typewriter").innerHTML += firstText.charAt(index);
+        index++;
+        setTimeout(typeWriter, speed);
+    }
+    else {
+        // hide the initial cursor after the firstText is fully typed out
+        document.getElementById("cursor").style.display = 'none';
+        showSecondText(); // Show all the second text lines simultaneously
+    }
+}
+
+function showSecondText() {
+    for (let i = 0; i < secondText.length; i++) {
+        const lineId = `line${i + 1}`;
+        document.getElementById(lineId).classList.remove('hidden');
+    }
+    document.getElementById("final-cursor").style.display = 'inline-block';
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(typeWriter, speed); // start the typewriter effect once the document is loaded
+});
