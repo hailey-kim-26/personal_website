be364f5b9980739baac9493dec7c9fb8181e9f84
hailey-kim-26 <haileyk0602@gmail.com>
2025-03-17 09:42:10 -0700

    Updates

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-/*
-    Typewriter effect and blinking cursor in Text line
-*/
 const firstText = "cat README";
 const secondText = [
     "Name: Hailey Kim",
     "Major: B.S. Computer Science",
     "Minor: Mathematics",
     "School: University of Nevada-Las Vegas",
+    "Download my resume",
+    "Visit my GitHub",
     "hailey@desktop:~$ "
-];
+  ];
 
-let index = 0; // track current position in the firstText
-const speed = 100; // speed in milliseconds for typing effect
+let index = 0;
+const speed = 100;
 
 function typeWriter() {
-    if (index < firstText.length) {
-        document.getElementById("typewriter").innerHTML += firstText.charAt(index);
-        index++;
-        setTimeout(typeWriter, speed);
-    }
-    else {
-        // hide the initial cursor after the firstText is fully typed out
-        document.getElementById("cursor").style.display = 'none';
-        showSecondText(); // Show all the second text lines simultaneously
-    }
+  if (index < firstText.length) {
+    document.getElementById("typewriter").innerHTML += firstText.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  } else {
+    document.getElementById("cursor").style.display = 'none';
+    showSecondText();
+  }
 }
 
 function showSecondText() {
-    for (let i = 0; i < secondText.length; i++) {
-        const lineId = `line${i + 1}`;
-        document.getElementById(lineId).classList.remove('hidden');
-    }
-    document.getElementById("final-cursor").style.display = 'inline-block';
+    setTimeout(() => {
+        for (let i = 0; i < secondText.length; i++) {
+            document.getElementById(`line${i + 1}`).classList.remove("hidden");
+        }
+        document.getElementById("final-cursor").style.display = 'inline-block';
+    }, 1000); // Delay of 1 second (1000ms) before showing all lines
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(typeWriter, speed); // start the typewriter effect once the document is loaded
-});
-
-document.querySelector('.mobile-icon').addEventListener('click', function() {
-    this.classList.toggle('open');
-    document.querySelector('.menu-links').classList.toggle('open');
+  setTimeout(typeWriter, speed);
 });
